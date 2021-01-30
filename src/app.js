337ffce34efd15be5a037e7a57cb902228e4878d
Yusuf Efe Yılmaz <66946818+potovzuc@users.noreply.github.com>
2021-01-30 21:38:00 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -78,7 +78,7 @@ client.on('message', (channel, userstate, message, self) => {
     return
   }
 
-	if(message.toLowerCase() === 'madzzyZz Gaijin madzzyZz Fred madzzyZz Nirenu madzzyZz VCan madzzyZz Yankas madzzyZz Gaijin madzzyZz Fred madzzyZz Nirenu madzzyZz VCan madzzyZz Yankas madzzyZz Gaijin madzzyZz Fred madzzyZz Nirenu madzzyZz VCan madzzyZz Gaijin madzzyZz Fred madzzyZz Nirenu madzzyZz VCan madzzyZz Yankas madzzyZz Gaijin madzzyZz Fred madzzyZz Nirenu madzzyZz VCan madzzyZz Yankas madzzyZz Gaijin madzzyZz Fred madzzyZz Nirenu madzzyZz VCan') {
+	if(message.toLowerCase() === '!hello') {
     hello(channel, userstate)
     return
   }
@@ -98,49 +98,39 @@ function onConnectedHandler(address, port) {
   console.log(`Connected: ${address}:${port}`)
 }
 
-
-//Bize host
-/*function onHostedHandler (channel, username, viewers, autohost) {
+function onHostedHandler (channel, username, viewers, autohost) {
   client.say(channel,
-    `@${username} ${viewers} kişi ile bizi onurlandırdı`
-  )
-}*/
-
-
-//Raid
-/*function onRaidedHandler(channel, username, viewers) {
-  client.say(channel,
-    `@${username} bize ${viewers} kişi ile savaş açtı Kappa`
-  )
-}*/
-
-
-//Abone olma
-function onSubscriptionHandler(channel, username, method, message, userstate) {
-  client.say(channel,
-    `madzzyHelal madzzyHelal madzzyHelal @${username} madzzyHelal madzzyHelal madzzyHelal`
+    `Thank you @${username} for the host of ${viewers}!`
   )
 }
 
-
-//Bit gönderimi
-/*function onCheerHandler(channel, userstate, message)  {
+function onRaidedHandler(channel, username, viewers) {
   client.say(channel,
-    `${userstate.username} ${userstate.bits} bit ne madzzyKalp`
+    `Thank you @${username} for the raid of ${viewers}!`
   )
-}*/
+}
 
-//Sub gift
-/*function onGiftPaidUpgradeHandler(channel, username, sender, userstate) {
+function onSubscriptionHandler(channel, username, method, message, userstate) {
   client.say(channel,
-    `${username} Hediye aboneliğinine devam etti madzzyKalp`
+    `Thank you @${username} for subscribing!`
   )
-}*/
+}
 
-//Host
+function onCheerHandler(channel, userstate, message)  {
+  client.say(channel,
+    `Thank you @${userstate.username} for the ${userstate.bits} bits!`
+  )
+}
+
+function onGiftPaidUpgradeHandler(channel, username, sender, userstate) {
+  client.say(channel,
+    `Thank you @${username} for continuing your gifted sub!`
+  )
+}
+
 function onHostingHandler(channel, target, viewers) {
   client.say(channel,
-    `${viewers} kişi ile ${target}'a hostlanıyoruz madzzyKalp`
+    `We are now hosting ${target} with ${viewers} viewers!`
   )
 }
 
@@ -148,38 +138,42 @@ function reconnectHandler () {
   console.log('Reconnecting...')
 }
 
-/*function resubHandler(channel, username, months, message, userstate, methods) {
+function resubHandler(channel, username, months, message, userstate, methods) {
   const cumulativeMonths = userstate['msg-param-cumulative-months']
   client.say(channel,
-    `@${username} ${cumulativeMonths} milyar milyor yıl ne madzzyHelal`
+    `Thank you @${username} for the ${cumulativeMonths} sub!`
   )
-}*/
+}
 
-
-//Sub gift
-/*function subGiftHandler(channel, username, streakMonths, recipient, methods, userstate) {
+function subGiftHandler(channel, username, streakMonths, recipient, methods, userstate) {
 
   client.say(channel,
-    `${username} ${recipient} 'a abonelik hediye etmiş madzzyKalp madzzyHelal `
+    `Thank you @${username} for gifting a sub to ${recipient}}.`
   )
-}*/
+
+  // this comes back as a boolean from twitch, disabling for now
+  // "msg-param-sender-count": false
+  // const senderCount =  ~~userstate["msg-param-sender-count"];
+  // client.say(channel,
+  //   `${username} has gifted ${senderCount} subs!`
+  // )
+}
 
 // commands
 
 function hello (channel, userstate) {
-  client.say(channel, `madzzyZz Gaijin madzzyZz Fred madzzyZz Nirenu madzzyZz VCan madzzyZz Yankas madzzyZz Gaijin madzzyZz Fred madzzyZz Nirenu madzzyZz VCan madzzyZz Yankas madzzyZz Gaijin madzzyZz Fred madzzyZz Nirenu madzzyZz VCan madzzyZz Gaijin madzzyZz Fred madzzyZz Nirenu madzzyZz VCan madzzyZz Yankas madzzyZz Gaijin madzzyZz Fred madzzyZz Nirenu madzzyZz VCan madzzyZz Yankas madzzyZz Gaijin madzzyZz Fred madzzyZz Nirenu madzzyZz VCan`)
+  client.say(channel, `@${userstate.username}, heya!`)
 }
 
-/*function checkTwitchChat(userstate, message, channel) {
+function checkTwitchChat(userstate, message, channel) {
   console.log(message)
   message = message.toLowerCase()
   let shouldSendMessage = false
   shouldSendMessage = BLOCKED_WORDS.some(blockedWord => message.includes(blockedWord.toLowerCase()))
   if (shouldSendMessage) {
     // tell user
-    client.say(channel, `@${userstate.username}, afedersiniz! Mesajınız silindi.`)
+    client.say(channel, `@${userstate.username}, sorry!  You message was deleted.`)
     // delete message
     client.deletemessage(channel, userstate.id)
   }
 }
-*/
